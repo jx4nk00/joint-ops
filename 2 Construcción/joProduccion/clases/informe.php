251357c1a9ba1dca8bddcc6f76e47d71aa8d0e35
juanka.rk@gmail.com <jx4nk00@users.noreply.github.com>
2013-09-24 03:34:27 +0000

    Se agrega carpeta para Producción de la aplicación, esta será modificada para posteriormente ser levanrtada a Calidad

---
+++ 2 Construcción/joProduccion/clases/informe.php
@@ -0,0 +1,21 @@
+<?php 
+include('conexion.php');
+class Informe{
+
+
+	function subirInforme($ruta, $code){
+
+		$consulta = mysql_query("INSERT INTO informes (ruta,cod_informe) VALUES ('$ruta','$code')");
+
+	}
+
+
+	function ultimoInforme(){
+		$consulta = mysql_query("SELECT id_informes FROM informes ORDER BY id_informes DESC");
+		$valor = mysql_fetch_array($consulta);
+
+		return $valor[0];
+	}
+
+}
+ ?>
