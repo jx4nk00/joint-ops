e09bcf7a03e9cd83ceef370f3c833fd79bdc9cfb
juanka.rk@gmail.com <juanka.rk@gmail.com@26d09ebd-1ac2-1130-b28e-15eff5c45a37>
2013-11-21 12:27:21 +0000

    Actualización de aplicación

---
+++ trunk/3 Construcción/Aplicación/clases/conexion.php
@@ -0,0 +1,11 @@
+<?php	
+	
+	/*
+	$conexion = mysql_connect ("localhost", "opizarro_jo2013", "alalito11"); //--> Desarrollo
+	mysql_select_db ("opizarro_jo_produccion", $conexion);
+	*/
+
+	$conexion = mysql_connect ("localhost","opservic_jo", "jointops2013"); //--> Produccion
+	mysql_select_db ("opservic_jo_desarrollo", $conexion);
+
+?>
